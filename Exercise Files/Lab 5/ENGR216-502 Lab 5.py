e42ef2ae6b45cf575e1d7b7d87ba7de1b134e534
cobeggs8735 <cobeggs@protonmail.com>
2025-05-16 19:15:36 -0500

    Add files via upload

---
+++ Exercise Files/Lab 5/ENGR216-502 Lab 5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 12:22:39 2019
+
+@author: colemanbeggs
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import statistics
+
